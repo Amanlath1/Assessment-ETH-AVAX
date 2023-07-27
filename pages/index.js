3e79149803e1ba16cdb259c5e9592f5a320e1c6f
aman <aman@mail.com>
2023-07-27 22:40:57 +0530

    FIles Added

---
+++ pages/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { ethers } from 'ethers';
+import SimpleWalletABI from '<PATH_OF_YOUR_CONTRACT_JSON_FILE>';// Replace with the actual path after compilation of smart contract
+
+const contractAddress = '<YOUR_CONTRACT_ADDRESS>'; // Replace with the actual contract address after deployment
+
+function App() {
+  const [amount, setAmount] = useState('');
+  const [balance, setBalance] = useState(0);
+
+  const connectToMetamask = async () => {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+    }
+  };
+
+  const getBalance = async () => {
+    if (!window.ethereum) return;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, SimpleWalletABI.abi, signer);
+
+    try {
+      const userBalance = await contract.getBalance();
+      setBalance(userBalance.toString());
+    } catch (error) {
+      console.error('Error fetching balance:', error);
+    }
+  };
+
+  const deposit = async () => {
+    if (!window.ethereum || !amount) return;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, SimpleWalletABI.abi, signer);
+
+    try {
+      const tx = await contract.deposit({ value: ethers.utils.parseEther(amount) });
+      await tx.wait();
+      setAmount('');
+      getBalance();
+    } catch (error) {
+      console.error('Error depositing funds:', error);
+    }
+  };
+
+  const withdraw = async () => {
+    if (!window.ethereum || !amount) return;
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, SimpleWalletABI.abi, signer);
+
+    try {
+      const tx = await contract.withdraw(ethers.utils.parseEther(amount));
+      await tx.wait();
+      setAmount('');
+      getBalance();
+    } catch (error) {
+      console.error('Error withdrawing funds:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Simple Wallet App</h1>
+      <button onClick={connectToMetamask}>Connect to Metamask</button>
+      <p>Account Balance: {balance} ETH</p>
+      <input type="text" value={amount} onChange={(e) => setAmount(e.target.value)} />
+      <button onClick={deposit}>Deposit</button>
+      <button onClick={withdraw}>Withdraw</button>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+  const SimpleWallet = await ethers.getContractFactory('SimpleWallet');
+  const simpleWallet = await SimpleWallet.deploy();
+  await simpleWallet.deployed();
+  console.log('SimpleWallet deployed to:', simpleWallet.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
